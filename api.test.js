c9c78c79834efa47f7333b89742d86fe07c014bb
borissofft <kolegata86@abv.bg>
2023-11-12 17:23:11 +0200

    Add first API test

---
+++ api.test.js
@@ -0,0 +1,34 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('./server');
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+
+describe("Books API Tests", () => {
+    let bookId;
+
+    it("should POST a book", (done) => {
+        const book = { id: "1", title: "Test Book", author: "Test Author" };
+
+        chai.request(server)
+            .post('/books')
+            .send(book)
+            .end((err, res) => {
+
+                if (err) {
+                    return done(err);
+                }
+
+                expect(res).to.have.status(201);
+                expect(res.body).to.be.a('object');
+                expect(res.body).to.have.property('id');
+                expect(res.body).to.have.property('title');
+                expect(res.body).to.have.property('author');
+                bookId = res.body.id;
+                done();
+            });
+    });
+
+});
